286aa1d184c237843f477d958ef37ef37a1f0d12
gris12eldaa <griseldafranco671@gmail.com>
2025-04-10 21:13:48 -0600

    fix: conexion a postgresql

--- config/db.py
+++ config/db.py
@@ -1,22 +1,22 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configuración de la base de datos
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/baseprueba"
+# Configuración de la base de datos PostgreSQL
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://root:3euzgylJZMZoTv7YbLPwNQqzyFxc5QEe@dpg-cvs8bauuk2gs739os6d0-a:5432/baseprueba_68wt"
 
-# Crear el motor (engine)
+# Crear el engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 # Crear la sesión
 SesionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Definir el objeto base para los modelos
+# Base para modelos
 Base = declarative_base()
 
 from models.materiales import Material
 from models.prestamos import Prestamo
 import models.user as User
 
-# Crear las tablas en orden específico
-#Material.__table__.create(bind=engine)
+# Para crear tablas manualmente:
+# Base.metadata.create_all(bind=engine)
