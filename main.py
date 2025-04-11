9282d6cafbbf02f85ef9df372c9cbd8439e7b52d
gris12eldaa <griseldafranco671@gmail.com>
2025-04-10 21:37:35 -0600

    fix: puerto de conexion

--- main.py
+++ main.py
@@ -31,3 +31,11 @@ def root():
 
 app.include_router(user)
 
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 10000)) 
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+
